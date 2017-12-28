a2f29f6183eb661adc39477249cfdaef877148a0
lautumn1990 <932034966@qq.com>
2017-12-29 00:09:26 +0800

    优化类命名结构

--- src/test/java/com/lautumn/lucene/WildcardQueryDemoTest.java
+++ src/test/java/com/lautumn/lucene/WildcardQueryDemoTest.java
@@ -8,33 +8,31 @@ import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
-import static org.testng.Assert.*;
-
 /**
  * Created by Administrator on 2017/12/28.
  */
-public class FuzzyQueryTestTest {
-    private static final Logger logger = LoggerFactory.getLogger(FuzzyQueryTestTest.class);
+public class WildcardQueryDemoTest {
+    private static final Logger logger = LoggerFactory.getLogger(WildcardQueryDemoTest.class);
     @Before
     public void initLog(){
 //        Property
     }
-    FuzzyQueryTest fuzzyQueryTest = new FuzzyQueryTest();
+    WildcardQueryDemo wildcardQueryDemo = new WildcardQueryDemo();
     @Test
     public void testGetPerson(){
-        List<Person> persons = fuzzyQueryTest.getPersons(10);
+        List<Person> persons = wildcardQueryDemo.getPersonList(10);
         System.out.println(persons);
     }
     @Test
     public void testPersonToIndex(){
-        logger.info("abc");
-        fuzzyQueryTest.personToIndex();
+//        logger.info("abc");
+        wildcardQueryDemo.personListToIndex();
     }
 
     @Test
     public void testSearchPerson(){
         String keyword = "uc";
-        List<Person> persons = fuzzyQueryTest.getPersonsByKeyword(keyword, 0, 100);
+        List<Person> persons = wildcardQueryDemo.searchPersonsByKeyword(keyword, 0, 100);
         System.out.println("<br/><br/><span style='color:red;'>关键字：：："+keyword+"</span>\n\n<br/><br/>");
         System.out.println("条数："+persons.size() +"\n\n"+"<br/>"+"<br/>");
         for (Person person : persons) {
